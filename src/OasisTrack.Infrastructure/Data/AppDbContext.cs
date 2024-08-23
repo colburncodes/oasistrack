0aaa3d3975ee268ca5155475a6a5bf64a77f4dad
colburncodes <colburnsanders@gmail.com>
2024-08-22 19:38:48 -0500

    new migration

--- src/OasisTrack.Infrastructure/Data/AppDbContext.cs
+++ src/OasisTrack.Infrastructure/Data/AppDbContext.cs
@@ -8,4 +8,21 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     
     public DbSet<Store> Stores { get; set; }
+    public DbSet<Route> Routes { get; set; }
+    public DbSet<Driver> Drivers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Route>()
+            .HasOne(r => r.Driver)
+            .WithOne(d => d.CurrentRoute)
+            .HasForeignKey<Route>(r => r.DriverId)
+            .IsRequired(false);
+        
+        builder.Entity<Driver>()
+            .HasOne(r => r.CurrentRoute)
+            .WithOne(d => d.Driver)
+            .HasForeignKey<Driver>(r => r.CurrentRouteId)
+            .IsRequired(false);
+    }
 }
